af0c984edd64c65b4ca3427fddc7e7009b7ce32f
Xiang-tao <1689415053@qq.com>
2021-08-11 23:06:57 +0800

    update

---
+++ Quick_start/pointer/build/src/CMakeFiles/size_pointer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/size_pointer.dir/size_pointer.cpp.o"
+  "size_pointer"
+  "size_pointer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/size_pointer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Quick_start/pointer/src/size_pointer.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+	int a = 10;
+
+	int * p;
+	p = &a; //指针指向数据a的地址
+
+	cout << *p << endl; //* 解引用
+	cout << sizeof(p) << endl;
+	cout << sizeof(char *) << endl;
+	cout << sizeof(float *) << endl;
+	cout << sizeof(double *) << endl;
+
+
+	return 0;
+}
